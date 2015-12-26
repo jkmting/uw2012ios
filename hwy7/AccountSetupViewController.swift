896a15a77b7777d7d6d4783c9e808b23e2530954
jkmting <jameskmting@gmail.com>
2015-12-26 12:28:16 -0500

    add init() to Costomer and CostomersQ class add a AccountSetupViewController that will setup the detial of the restaurant object. (This View should only showup on the first login. Create CostomersQ and add to a CostomersQList array. assign that CostomersQList to the restaurant Object.

---
+++ hwy7/AccountSetupViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AccountSetupViewController.swift
+//  hwy7
+//
+//  Created by Cindy Zhao on 2015-12-25.
+//  Copyright © 2015 Cindy Zhao. All rights reserved.
+//
+
+import Cocoa
+
+class AccountSetupViewController: UIViewController {
+
+}
